95776bd26c30463282b83f97c549d059e20ed136
raviranchod <raviranchod@gmail.com>
2021-09-29 22:59:06 +0100

    feat: styled-components install and setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
+import { GlobalStyle } from "../theme/global";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
+
+export default MyApp;
